1f2145421c96af6645accba806be632e13a11f0f
Victor <afra55@foxmail.com>
2020-08-28 16:28:47 +0800

    测试表盘1

--- app/src/main/java/com/afra55/speedometer/MainActivity.kt
+++ app/src/main/java/com/afra55/speedometer/MainActivity.kt
@@ -2,10 +2,35 @@ package com.afra55.speedometer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val speedometerAdapter = SpeedometerAdapter(this)
+        speedometer_vp.adapter = speedometerAdapter
+        speedometer_vp.offscreenPageLimit = speedometerAdapter.itemCount
+        speedometer_vp_indicator.attach(speedometer_vp)
+    }
+}
+
+
+class SpeedometerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+
+    val itemList by lazy {
+        val list = mutableListOf<Int>()
+        list.add(R.layout.view_speedometer_1)
+        list
+    }
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+    override fun createFragment(position: Int): Fragment {
+        return SpeedometerDialogFragment(itemList[position])
     }
 }
---
+++ app/src/main/java/com/afra55/speedometer/SpeedometerDialogFragment.kt
@@ -0,0 +1,68 @@
+package com.afra55.speedometer
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.util.toRange
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_speedometer_dialog.*
+
+/**
+ * @author Afra55
+ * @date 2020/8/28
+ * A smile is the best business card.
+ * 没有成绩，连呼吸都是错的。
+ */
+class SpeedometerDialogFragment(val speedometerDialogResId: Int) : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_speedometer_dialog, container, false)
+    }
+
+    val testHandler:Handler by lazy {
+        Handler(Handler.Callback {
+            mySpeedometerDialog?.setCurrentNumber((0..180).random().toFloat())
+            testHandler.sendEmptyMessageDelayed(0, 500)
+            false
+
+        })
+    }
+
+
+    var mySpeedometerDialog: SpeedometerDialog? = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val view = LayoutInflater.from(context).inflate(speedometerDialogResId, null, false)
+        val test_speedometer: View? = view.findViewById<View>(R.id.test_speedometer)
+        if (test_speedometer is SpeedometerDialog) {
+            mySpeedometerDialog = test_speedometer
+            item_root.addView(view, 0)
+            mySpeedometerDialog!!.setLimitNumber(80)
+            mySpeedometerDialog!!.setMaxNumber(180F)
+
+            testHandler.sendEmptyMessage(0)
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (mySpeedometerDialog == null) {
+
+        }
+
+    }
+
+    override fun onDetach() {
+        testHandler.removeMessages(0)
+        super.onDetach()
+    }
+}
+
